7e9b2b780d756ee8b0fc04fb62d4a9fb067a8442
Ariya Panna <ariyapanna@outlook.com>
2026-05-15 11:40:23 +0700

    refactor: application retry 10 times to connect to database if fail instead of instantly stop

--- backend/internal/infrastructure/database/postgres.go
+++ backend/internal/infrastructure/database/postgres.go
@@ -1,15 +1,26 @@
 package database
 
 import (
+	"log"
+	"time"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewPostgresDB(dsn string) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, err
+	var db *gorm.DB
+	var err error
+
+	for i := 0; i < 10; i++ {
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			return db, nil
+		}
+
+		log.Printf("Failed to connect to database, retrying in 2 seconds... (%d/10)", i+1)
+		time.Sleep(2 * time.Second)
 	}
 
-	return db, nil
-}
+	return nil, err
+}
